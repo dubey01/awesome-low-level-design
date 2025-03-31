923ae021a82ccc8ccdb406248c07ebc404ab17ce
Arjun Dubey <arjundjsce01@gmail.com>
2025-03-31 16:37:45 +0530

    Comments for understanding LLD 1.0

--- solutions/java/src/parkinglot/ParkingSpot.java
+++ solutions/java/src/parkinglot/ParkingSpot.java
@@ -5,8 +5,11 @@ import parkinglot.vehicletype.VehicleType;
 
 public class ParkingSpot {
     private final int spotNumber;
+    // To identify a unique spot
     private final VehicleType vehicleType;
+    // To identify the type of vehicle that can be parked in this spot
     private Vehicle parkedVehicle;
+    // Which vehicle has been parked currently in this spot
 
     public ParkingSpot(int spotNumber, VehicleType vehicleType) {
         this.spotNumber = spotNumber;
